c9ace49426c6dd9a677b893aa34d4ec9774b5cb2
avi92 <axelvirnich@googlemail.com>
2016-01-11 17:40:11 +0100

    Change method for distance calculation

--- src/de/ifgi/algorithm/LandmarksIncluder.java
+++ src/de/ifgi/algorithm/LandmarksIncluder.java
@@ -7,17 +7,18 @@ import java.util.Iterator;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.geotools.geometry.jts.JTS;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.opengis.feature.simple.SimpleFeature;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.impl.CoordinateArraySequenceFactory;
 
 import de.ifgi.landmark.Landmark;
 
@@ -33,7 +34,14 @@ public class LandmarksIncluder {
 		roads.forEach(road -> {
 			for (Iterator<Landmark> iterator = landmarks.iterator(); iterator.hasNext(); ) {
 				Landmark landmark = iterator.next();
-				double distance = landmark.getLocation().distance(road);
+//				double distance = landmark.getLocation().distance(road);
+				double distance = 0;
+				try {
+					distance = JTS.orthodromicDistance(road.getCoordinates()[road.getNumGeometries() / 2], landmark.getLocation().getCoordinate(), ((SimpleFeature)road.getUserData()).getType().getCoordinateReferenceSystem());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				System.out.println("Landmark Distance: " + distance);
 				if (distance < landmark.getRadius()) {
 					result.add(landmark);
@@ -48,7 +56,7 @@ public class LandmarksIncluder {
 	
 	private ArrayList<Landmark> loadLandmarks() {
 		ArrayList<Landmark> result = new ArrayList<Landmark>();
-		GeometryFactory gf = new GeometryFactory();
+		GeometryFactory gf = JTSFactoryFinder.getGeometryFactory();
 		
 		try {
 			File xmlFile = new File("src/res/landmarks.xml");
@@ -68,9 +76,10 @@ public class LandmarksIncluder {
 					String category = element.getElementsByTagName("category").item(0).getTextContent();
 					double lat = Double.valueOf(element.getElementsByTagName("lat").item(0).getTextContent());
 					double lng = Double.valueOf(element.getElementsByTagName("lng").item(0).getTextContent());
-					Coordinate[] coordinates = {new Coordinate(lat, lng)};
-					CoordinateSequence cs = CoordinateArraySequenceFactory.instance().create(coordinates);
-					Point location = new Point(cs, gf);
+//					Coordinate[] coordinates = {new Coordinate(lat, lng)};
+//					CoordinateSequence cs = CoordinateArraySequenceFactory.instance().create(coordinates);
+//					Point location = new Point(cs, gf);
+					Point location = gf.createPoint(new Coordinate(lng, lat));
 					double radius = Double.valueOf(element.getElementsByTagName("radius").item(0).getTextContent());
 					
 					result.add(new Landmark(name, category, location, radius));
